ff7d54f001fdefdeb4657ab26fe6c0f48fecd087
DEEBAKSENTHIL <35653078+DEEBAKSENTHIL@users.noreply.github.com>
2018-02-04 09:08:13 +0000

    Create power.c

---
+++ power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the numbers:");
+    scanf("%d%d",&a,&b);
+    c=pow(a,b);
+    printf("the value of a power b is %d",c);
+getch();
+}
