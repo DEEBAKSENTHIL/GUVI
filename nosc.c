f3ea86bcbc06f37667d27fa49493abbddbd3deac
DEEBAKSENTHIL <35653078+DEEBAKSENTHIL@users.noreply.github.com>
2018-03-08 16:48:17 +0530

    Create nosc.c

---
+++ nosc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int i,n=0;
+printf("Enter the string");
+scanf("%[^\n]s,&a);
+for(i=0;a[i]!='\0';i++)
+{
+if(!(a[i]>='0' && a[i]<='9' || a[i]>='a' && a[i]<='z'))
+{
+n=n+1;
+}
+}
+printf("%d",n);
+}
