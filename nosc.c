be77fd3970c8b2c23637fd92e1c5c901406c9cd6
DEEBAKSENTHIL <35653078+DEEBAKSENTHIL@users.noreply.github.com>
2018-03-08 16:48:40 +0530

    Update nosc.c

--- nosc.c
+++ nosc.c
@@ -4,7 +4,7 @@ void main()
 char a[100];
 int i,n=0;
 printf("Enter the string");
-scanf("%[^\n]s,&a);
+scanf("%[^\n]s",&a);
 for(i=0;a[i]!='\0';i++)
 {
 if(!(a[i]>='0' && a[i]<='9' || a[i]>='a' && a[i]<='z'))
