865dc513665ac931dd821016fb014d16f7d155b9
DEEBAKSENTHIL <35653078+DEEBAKSENTHIL@users.noreply.github.com>
2018-03-15 16:19:46 +0530

    Create guvi43.c

---
+++ guvi43.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char i,a[100],b[100],c[100];
+printf("Enter the string a : ");
+scanf("%s",a);
+printf("Enter the string b : ");
+scanf("%s",b);
+printf("%s%s",a,b);
+}
